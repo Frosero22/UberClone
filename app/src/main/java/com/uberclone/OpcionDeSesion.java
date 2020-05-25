73a856e2c125e5e0db3a4837ee667350d4b75676
Frosero22 <francesco.villacis@gmail.com>
2020-05-24 22:38:07 -0500

    Diseños - Falta Pantalla Registro y Login

---
+++ app/src/main/java/com/uberclone/OpcionDeSesion.java
@@ -0,0 +1,14 @@
+package com.uberclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpcionDeSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opcion_de_sesion);
+    }
+}
